6011d52980a69840c0a5b3239a8cf76b36a743bb
junaidbinjaman <me@junaidbinjaman.com>
2026-03-07 15:28:48 +0600

    feat: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+    title: 'App Router',
+    description:
+        'App Router is the new file-based router in Next.js 13. It provides a more intuitive and powerful way to define routes and layouts in your application.',
+};
+
+export default function Page() {
+    return (
+        <section>
+            <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+        </section>
+    );
+}
